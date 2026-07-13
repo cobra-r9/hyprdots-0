be66c133bcbe37ef7015f61758cc59a41f30d605
cobra-r9 <cobra.rev.9@gmail.com>
2026-07-13 09:18:37 +0530

    initial config

---
+++ dot_config/hypr/variables.lua
@@ -0,0 +1,121 @@
+local scheme = require("scheme.current")
+
+return {
+    ------------------
+    ---- HYPRLAND ----
+    ------------------
+
+    -- Apps
+    terminal                   = "foot",
+    browser                    = "zen-browser",
+    editor                     = "nvim",
+    fileExplorer               = "thunar",
+    audioSettings              = "pavucontrol",
+
+    -- Touchpad
+    touchpadDisableTyping      = true,
+    touchpadScrollFactor       = 0.3,
+    gestureFingers             = 3,
+    workspaceSwipeFingers      = 3,
+    gestureFingersMore         = 4,
+    gestureFingersFullscreen   = 3,
+
+    -- Blur
+    blurEnabled                = true,
+    blurSpecialWs              = false,
+    blurPopups                 = true,
+    blurInputMethods           = true,
+    blurSize                   = 8,
+    blurPasses                 = 2,
+    blurXray                   = false,
+
+    -- Shadow
+    shadowEnabled              = true,
+    shadowRange                = 15,
+    shadowRenderPower          = 4,
+    shadowColour               = "rgba(" .. scheme.inversePrimary .. "10)",
+
+    -- Gaps
+    workspaceGaps              = 20,
+    windowGapsIn               = 5,
+    windowGapsOut              = 10,
+    singleWindowGapsOut        = 10,
+
+    -- Window styling
+    windowOpacity              = 0.95,
+    windowRounding             = 15,
+    windowBorderSize           = 1,
+    activeWindowBorderColour   = "rgba(" .. scheme.primary .. "e6)",
+    inactiveWindowBorderColour = "rgba(" .. scheme.onSurfaceVariant .. "11)",
+
+    -- Misc
+    volumeStep                 = 10,
+    volumeMax                  = 100,
+    cursorTheme                = "sweet-cursors",
+    cursorSize                 = 24,
+    sleepGestureCmd            = "systemctl suspend-then-hibernate",
+
+    ------------------
+    ---- KEYBINDS ----
+    ------------------
+
+    -- Workspaces
+    kbMoveWinToWs              = "SUPER + SHIFT",
+    kbMoveWinToWsGroup         = "CTRL + SUPER + ALT",
+    kbGoToWs                   = "SUPER",
+    kbGoToWsGroup              = "CTRL + SUPER",
+    kbNextWs                   = "CTRL + SUPER + Right",
+    kbPrevWs                   = "CTRL + SUPER + Left",
+    kbToggleWsLayout           = "f12",
+    kbToggleWsScrollLayout     = "SUPER + Space",
+
+    -- Window Group
+    kbWindowGroupCycleNext     = "ALT + TAB",
+    kbWindowGroupCyclePrev     = "SHIFT + ALT + TAB",
+    kbUngroup                  = "SUPER + U",
+    kbToggleGroup              = "SUPER + Comma",
+
+    -- Window Action
+    kbMoveWindow               = "SUPER + Z",
+    kbResizeWindow             = "SUPER + X",
+    kbWindowPip                = "f8",
+    kbPinWindow                = "f9",
+    kbWindowFullscreen         = "f10",
+    kbWindowBorderedFullscreen = "SUPER + ALT + F",
+    kbToggleWindowFloating     = "f11",
+    kbCloseWindow              = "SUPER + C",
+
+    -- Special workspaces toggles
+    kbSpecialWs                = "SUPER + S",
+    kbSystemMonitorWs          = "CTRL + SHIFT + Escape",
+    kbMusicWs                  = "SUPER + M",
+    kbCommunicationWs          = "SUPER + D",
+    kbTodoWs                   = "SUPER + R",
+
+    -- Apps
+    kbTerminal                 = "SUPER + Return",
+    kbBrowser                  = "SUPER + B",
+    kbEditor                   = "SUPER + E",
+    kbFileExplorer             = "SUPER + T",
+
+    -- Misc
+    kbSession                  = "ALT + f4",
+    kbShowSidebar              = "SUPER + N",
+    kbClearNotifs              = "CTRL + ALT + C",
+    kbShowPanels               = "SUPER + K",
+    kbLock                     = "SUPER + L",
+    kbRestoreLock              = "SUPER + ALT + L",
+
+    -- Caelestia
+    kbKillCaelestia            = "CTRL + ALT + Delete",
+    kbRestartCaelestia         = "CTRL + SHIFT + R",
+
+    -- Float Rules and app list. 
+    floatRules = {
+        ["thunar"] = { w = 1000, h = 600, x = 100, y = 100 },
+        ["foot"] = { w = 1000, h = 600 }, --> no x, y : just centers
+        ["org.kde.dolphin"] = { w = 1000, h = 800, x = 200, y = 200},
+    },
+
+    defaultRule = { w = 1000, h = 600 },
+}
---
+++ dot_config/private_fish/private_conf.d/aliases.fish
@@ -0,0 +1,26 @@
+# ~/.config/fish/conf.d/aliases.fish
+#
+if test "$TERM" = linux
+    alias l='ls'
+    alias ll='ls -l'
+    alias la='ls -la'
+else
+    alias ls='eza --ignore-glob="\$RECYCLE.BIN|System Volume Information"  --icons auto'
+    alias ll='eza --ignore-glob="\$RECYCLE.BIN|System Volume Information"  --icons auto -l'
+    alias la='eza --ignore-glob="\$RECYCLE.BIN|System Volume Information"  --icons auto -la'
+
+end
+
+alias grep='grep --color=auto'
+alias cdir='cd ~/Workspace/c'
+alias git_downloads='cd ~/Downloads/git'
+alias workspace='cd ~/Workspace'
+alias github='cd ~/Workspace/github'
+
+# git aliases
+alias gs='git switch'
+alias gb='git branch'
+alias ga='git add .'
+alias gc='git commit -m'
+alias gm='git merge'
+alias gp='git push'
