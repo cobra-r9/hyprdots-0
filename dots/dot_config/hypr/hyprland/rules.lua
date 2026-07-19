6ced9cd11f6debc3a74206e3043697fed8ce605f
cobra-r9 <cobra.rev.9@gmail.com>
2026-07-19 12:42:35 +0530

    udpated dots

--- dots/dot_config/hypr/hyprland/rules.lua
+++ dots/dot_config/hypr/hyprland/rules.lua
@@ -116,7 +116,6 @@ hl.window_rule({
 hl.window_rule({ match = { initial_title = "Spotify( %(?Free%)?)?" }, workspace = "special:music" }) -- Spotify wayland, it has no class for some reason
 hl.window_rule({ match = { class = "discord|equibop|vesktop|whatsapp" }, workspace = "special:communication" })
 hl.window_rule({ match = { class = "Todoist" }, workspace = "special:todo" })
-
 -------------------------
 ---- Workspace rules ----
 -------------------------
--- dots/dot_config/private_fish/private_conf.d/starship.fish
+++ dots/dot_config/private_fish/private_conf.d/starship.fish
@@ -1,1 +1,8 @@
-starship init fish | source
+# Detect TTY vs GUI
+
+if test "$TERM" = linux
+    echo ""
+else
+    set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+    starship init fish | source
+end
